91bc31c2c8906e0bbcd72bbb6fc8cf751fd3372d
LeroyVincent <vincentleroy.web@gmail.com>
2025-01-20 13:20:30 +0100

    Fix menu initialisation. Add DisableOnCallback

---
+++ Runtime/Scripts/Misc/DisableOnCallback.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace ModularOptions {
+	/// <summary>
+	/// Little utility script for disabling Options Menu in common Unity callback types.
+	/// </summary>
+	[AddComponentMenu("Modular Options/Misc/Disable On Callback")]
+	public class DisableOnCallback : MonoBehaviour {
+
+
+		public UnityCallbackMessageType callbacks;
+		void Awake()
+		{
+			if (callbacks == UnityCallbackMessageType.Awake)
+				gameObject.SetActive(false);
+		}
+
+		void Start(){
+			if(callbacks == UnityCallbackMessageType.Start)
+				gameObject.SetActive(false);
+		}
+
+		void OnEnable()
+		{
+			if (callbacks == UnityCallbackMessageType.OnEnable)
+				gameObject.SetActive(false);
+		}
+	}
+
+	[Flags]
+	public enum UnityCallbackMessageType
+    {
+		Awake = 1,
+		Start = 2,
+		OnEnable = 4
+    }
+}
